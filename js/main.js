ae496fc130a65fa199c73581e2547c0941260a98
ValerioGc <valerio.gc1@gmail.com>
2022-07-15 18:09:59 +0200

    Avanzamento

--- js/main.js
+++ js/main.js
@@ -1,20 +1,51 @@
 // Numeri da generare
 const numberQ = 5;
+let points = 0;
 
 // Array numeri pc e numeri utente
 const arrNumb = randNumberGen(numberQ, 1, 100);
 console.log(`Array numeri PC = ${arrNumb}`);
 
-// Generazione contenitore numeri
-let htmlEl = document.getElementById('numberContainer');
-for (let i = 0; i < arrNumb.length; i++) {
-    htmlEl.innerHTML += `<li>${arrNumb[i]}</li>`;
-}
-
-
 
 const user_numbers = usrArrNumb();
 console.log(`Array numeri utente inseriti = ${user_numbers}`);
+
+
+// Generazione contenitore numeri
+let htmlEl = document.getElementById('numberContainer');
+for (let i = 0; i < arrNumb.length; i++) {
+    htmlEl.innerHTML += `<li class="list">${arrNumb[i]}</li>`;
+}
+// Timeout per nascondere numeri pc
+// let primoTO = setTimeout(newStep, 30000);
+let secondoT0 = setTimeout(hid, 5000);
+
+// TimeOut inserimento numeri 
+let primoTO = setTimeout(usrArrNumb, 5200);
+
+
+
+let sub = document.querySelector('button');
+sub.addEventListner('click', function () {
+    alert('bottone cliccato')
+    for (let i = 0; i < numberQ; i++) {
+        console.log(`${i}`)
+        console.log(`${points}`)
+        console.log(`${arrNumb}`)
+        console.log(`${user_numbers}`)
+        if (arrNumb.include(i)) {
+            points += 1;
+            alert('ao')
+        }
+    }
+});
+console.log(points);
+
+
+
+
+
+
 
 
 // // Generatore numeri unici randomici - restituisce array numeri
@@ -24,26 +55,24 @@ function randNumberGen(numberQuantity, min, max) {
         let condition = true;
         let randNumber = Math.floor(Math.random() * (max - min) + min);
         while (condition == true) {
-        // Verifica doppione
+            // Verifica doppione
             if (!arrayRand.includes(randNumber)) {
                 arrayRand.push(randNumber);
             } else {
                 condition = false;
             }
         }
-    }  
+    }
     return arrayRand;
 }
-
-
-
 // Funzione aggiunta numeri ad array numeri utente - restituisce array Numeri inseriti dall'utente
-function usrArrNumb () {
+function usrArrNumb() {
+    htmlEl.innerHTML = '';
     const usr_numb = [];
     for (let i = 0; i < 5; i++) {
-    //Acquisisco input utente
+        //Acquisisco input utente
         let user_input = parseInt(prompt('Inserisci numero:'));
-    // Aggiunge numeri ad Array numeri 
+        // Aggiunge numeri ad Array numeri
         usr_numb.push(user_input);
     }
     return usr_numb;
@@ -52,4 +81,17 @@ function usrArrNumb () {
 
 
 
+// // Funzione trasformazione contenitore numeri in input 
+// function newStep() {
+//     for (let i = 0; i < arrNumb.length; i++) {
+//         let exem = document.querySelector('.list');
+//         exem.className = 'd-none';
+//         htmlEl.innerHTML += `<input class="userN"></input>`;
+//     }
+// }
 
+
+function hid() {
+    htmlEl.classList = 'd-none';
+    document.getElementById('submit').classList.remove('d-none');
+}
