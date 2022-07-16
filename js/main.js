4d5d5bc97b39994c48c76786a1c476d537ec4828
ValerioGc <valerio.gc1@gmail.com>
2022-07-16 22:08:54 +0200

    Completamento esercizio

--- js/main.js
+++ js/main.js
@@ -1,54 +1,60 @@
+// Elementi DOM
+const htmlEl = document.getElementById('numberContainer');
+const subTitle = document.querySelector('h2');
+const subBtn = document.getElementById('btn');
+// Dichiaro variabile punteggio
+let points = 0;
 // Numeri da generare
 const numberQ = 5;
-let points = 0;
-
-// Array numeri pc e numeri utente
+// Array numeri pc
 const arrNumb = randNumberGen(numberQ, 1, 100);
 console.log(`Array numeri PC = ${arrNumb}`);
-
-
-const user_numbers = usrArrNumb();
-console.log(`Array numeri utente inseriti = ${user_numbers}`);
-
-
-// Generazione contenitore numeri
-let htmlEl = document.getElementById('numberContainer');
-for (let i = 0; i < arrNumb.length; i++) {
-    htmlEl.innerHTML += `<li class="list">${arrNumb[i]}</li>`;
-}
-// Timeout per nascondere numeri pc
-// let primoTO = setTimeout(newStep, 30000);
-let secondoT0 = setTimeout(hid, 5000);
-
-// TimeOut inserimento numeri 
-let primoTO = setTimeout(usrArrNumb, 5200);
-
-
-
-let sub = document.querySelector('button');
-sub.addEventListner('click', function () {
-    alert('bottone cliccato')
-    for (let i = 0; i < numberQ; i++) {
-        console.log(`${i}`)
-        console.log(`${points}`)
-        console.log(`${arrNumb}`)
-        console.log(`${user_numbers}`)
-        if (arrNumb.include(i)) {
-            points += 1;
-            alert('ao')
+// Event Listner bottone controllo tra array per Punti
+subBtn.addEventListener('click',
+    function () {
+        if (subBtn.innerHTML == 'Controlla Punteggio') {
+            controlPoint();
+        }
+        else if (subBtn.innerHTML == 'Riavvia Partita') {
+            location.reload();
+        }
+        else if (subBtn.innerHTML == 'Avvia') {
+            subBtn.classList.add('d-none')
+            subBtn.innerHTML = 'Controlla Punteggio';
+            htmlEl.classList.remove('d-none');
+        // Avvio timer
+            timerX(0, 30);
+        // Generazione contenitore numeri
+            for (let i = 0; i < arrNumb.length; i++) {
+                htmlEl.innerHTML += `<li class="bounce b-standard">${arrNumb[i]}</li>`;
+            }
+        // Nascondi numeri prima dell'alert
+            // let primoTO = setTimeout(newStep, 30000);
+            let secondoT0 = setTimeout(hid, 30000);
+        // TimeOut inserimento numeri
+            let primoTO = setTimeout(usrArrNumb, 32000);
         }
     }
-});
-console.log(points);
-
-
-
-
-
-
-
-
-// // Generatore numeri unici randomici - restituisce array numeri
+);
+/***********************************************
+******************  FUNZIONI *******************
+***********************************************/
+// Funzione timer Gioco
+function timerX(min, max) {
+    let lastT = document.getElementById('countd');
+    let time = max;
+    setInterval(function () {
+        if (time == min) {
+            clearInterval();
+        }
+        time--;
+        lastT.innerHTML = time;
+        if (time <= (max / 2)) {
+            lastT.style.color = 'red';
+        }
+    }, 1000);
+}
+// Generatore numeri unici randomici - restituisce array numeri
 function randNumberGen(numberQuantity, min, max) {
     const arrayRand = [];
     for (let i = 0; i < numberQuantity; i++) {
@@ -69,29 +75,51 @@ function randNumberGen(numberQuantity, min, max) {
 function usrArrNumb() {
     htmlEl.innerHTML = '';
     const usr_numb = [];
-    for (let i = 0; i < 5; i++) {
-        //Acquisisco input utente
+    for (let i = 0; i < numberQ; i++) {
+    //Acquisisco input utente
         let user_input = parseInt(prompt('Inserisci numero:'));
-        // Aggiunge numeri ad Array numeri
+    // Aggiunge numeri ad Array numeri
         usr_numb.push(user_input);
     }
     return usr_numb;
 }
-
-
-
-
-// // Funzione trasformazione contenitore numeri in input 
+//Funzione modifica elementi DOM
+function hid() {
+    htmlEl.classList = 'd-none';
+    document.getElementById('btn').classList.remove('d-none');
+    subTitle.innerText = '';
+}
+// Funzione trasformazione contenitore numeri in input
 // function newStep() {
 //     for (let i = 0; i < arrNumb.length; i++) {
-//         let exem = document.querySelector('.list');
-//         exem.className = 'd-none';
+//         let listN = document.querySelector('ul li');
+//         listN.className = 'd-none';
 //         htmlEl.innerHTML += `<input class="userN"></input>`;
 //     }
 // }
-
-
-function hid() {
-    htmlEl.classList = 'd-none';
-    document.getElementById('submit').classList.remove('d-none');
+// Funzione confronto numeri per punteggio
+function controlPoint() {
+// Definisco array numeri inseriti dall'utente
+    const user_numbers = usrArrNumb();
+    console.log(`Array numeri utente inseriti = ${user_numbers}`);
+    for (let i = 0; i < numberQ; i++) {
+    // Verifica doppione
+        if (!arrNumb.includes(user_numbers[i])) {
+            points = points;
+            console.log(`Numero ${i} NON incluso nell array numeri Utente`);
+        } else {
+            points += 1;
+            console.log(`Numero ${i} incluso nell array numeri PC`);
+            console.log(`Punteggio = points`);
+        }
+    }
+    console.log(`Punteggio finale = points`);
+    subTitle.classList.add('sc-h2');
+    subTitle.innerHTML = `Hai Indovinato <span id="pt" class="bold">${points}</span> Numeri!`;
+    subBtn.innerHTML = `Riavvia Partita`;
+    if (points == 0) {
+        document.getElementById('pt').style.color = 'red';
+    } else {
+        document.getElementById('pt').style.color = 'green';
+    }
 }
